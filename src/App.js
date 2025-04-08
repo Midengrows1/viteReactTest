6869b3625fe9a8fc85bf52be185db9dc38d687a9
Midengrows1 <midengrows@outlook.com>
2025-04-08 22:57:14 +0500

    Create an App.js file with core Reactjs

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+    const [count, setCount] = useState(0);
+    return /*#__PURE__*/ React.createElement(
+      "div",
+      null,
+      /*#__PURE__*/ React.createElement("p", null, "Hello World!")
+    );
+  }
+  export default App;
