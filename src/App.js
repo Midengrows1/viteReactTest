a362a77d0eee60535a4c57489a16685ea0108567
Midengrows1 <midengrows@outlook.com>
2025-04-14 09:54:40 +0500

    modify App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,71 @@
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 function App() {
     const [count, setCount] = useState(0);
+    const currentDate = new Date().toLocaleDateString();
     return /*#__PURE__*/ React.createElement(
-      "div",
-      null,
-      /*#__PURE__*/ React.createElement("p", null, "Hello World!")
+        React.Fragment,
+        null,
+    /*#__PURE__*/ React.createElement(
+            "div",
+            null,
+      /*#__PURE__*/ React.createElement(
+                "a",
+                {
+                    href: "https://vite.dev",
+                    target: "_blank"
+                },
+        /*#__PURE__*/ React.createElement("img", {
+                    src: viteLogo,
+                    className: "logo",
+                    alt: "Vite logo"
+                })
+            ),
+      /*#__PURE__*/ React.createElement(
+                "a",
+                {
+                    href: "https://react.dev",
+                    target: "_blank"
+                },
+        /*#__PURE__*/ React.createElement("img", {
+                    src: reactLogo,
+                    className: "logo react",
+                    alt: "React logo"
+                })
+            )
+        ),
+    /*#__PURE__*/ React.createElement("h1", null, "Vite + React"),
+    /*#__PURE__*/ React.createElement(
+            "div",
+            {
+                className: "card"
+            },
+      /*#__PURE__*/ React.createElement(
+                "button",
+                {
+                    onClick: () => setCount((count) => count + 1)
+                },
+                "count is ",
+                count
+            ),
+      /*#__PURE__*/ React.createElement(
+                "p",
+                null,
+                "Edit ",
+        /*#__PURE__*/ React.createElement("code", null, "src/App.jsx"),
+                " and save to test HMR"
+            )
+        ),
+    /*#__PURE__*/ React.createElement(
+            "p",
+            {
+                className: "read-the-docs"
+            },
+            "Click on the Vite and React logos to learn more"
+        ),
+        currentDate
     );
-  }
-  export default App;
+}
+export default App;
